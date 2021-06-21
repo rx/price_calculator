6c4db5bb9cfe056290132fce631a8c33c9566e8e
Russell Edens <russell@voomify.com>
2021-06-21 09:15:15 -0400

    fix: error handling with suggestions
    Error handling wasn't hooked up fully after the refactor from V1

--- lib/price_calculator/input_validator.rb
+++ lib/price_calculator/input_validator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'response'
+require_relative 'input_suggestions'
 
 module PriceCalculator
   # Takes an +Array+ of valid inventory items and validates supplied groceries against the valid inventory
@@ -16,7 +17,15 @@ module PriceCalculator
     def validate(*groceries)
       invalid_items = groceries - @inventory_items
       Response.new(data: groceries - invalid_items,
-                   errors: invalid_items)
+                   errors: build_error_messages(invalid_items))
+    end
+
+    def build_error_messages(invalid_items)
+      suggestor = PriceCalculator::InputSuggestions.new(@inventory_items)
+      invalid_items.map do |unrecognized_item|
+        suggestions = suggestor.suggest(unrecognized_item)
+        "Unrecognized item #{unrecognized_item}. #{suggestions.any? ? "did you mean '#{suggestions.join('or')}'?" : nil}"
+      end
     end
   end
 end
--- lib/price_calculator/models/inventory_item.rb
+++ lib/price_calculator/models/inventory_item.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'base'
 require_relative '../repositories/discounts'
 
 module PriceCalculator
